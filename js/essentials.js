7ff2127848c33098083af325aa02ccb7687d4ab3
nsomatrix <nsomatrix@yahoo.com>
2025-09-12 23:21:30 +0530

    fixed-navbar

--- js/essentials.js
+++ js/essentials.js
@@ -1,7 +1,10 @@
 // essentials.js
 $(function () {
     // Load the navbar dynamically
-    $('#navbar-placeholder').load('assets/navbar.html');
+    $('#navbar-placeholder').load('assets/navbar.html', function() {
+        // Load navbar functionality after navbar is loaded
+        $.getScript('js/navbar.js');
+    });
 
     // Load the footer dynamically
     $('#footer-placeholder').load('assets/footer.html');
---
+++ js/navbar.js
@@ -0,0 +1,105 @@
+// Navbar functionality
+$(document).ready(function() {
+    // Set active page based on current URL
+    setActivePage();
+    
+    
+    // Smooth scrolling for anchor links
+    initSmoothScrolling();
+    
+    // Close mobile menu when clicking outside
+    initMobileMenuClose();
+});
+
+function setActivePage() {
+    const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+    
+    // Remove active class from all nav items
+    $('.navbar-nav > li').removeClass('active');
+    
+    // Add active class to current page
+    $('.navbar-nav > li > a[href*="' + currentPage + '"]').parent().addClass('active');
+    
+    // Special handling for dropdown items
+    if (currentPage === 'emulators.html' || currentPage === 'mods.html' || currentPage === 'docs.html') {
+        $('.navbar-nav > li.dropdown:first').addClass('active');
+    } else if (currentPage === 'gallery.html' || currentPage === 'library.html' || currentPage === 'archives.html') {
+        $('.navbar-nav > li.dropdown:last').addClass('active');
+    }
+}
+
+
+function initSmoothScrolling() {
+    $('a[href^="#"]').click(function(e) {
+        e.preventDefault();
+        const target = $(this.getAttribute('href'));
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top - 70
+            }, 500);
+        }
+    });
+}
+
+function initMobileMenuClose() {
+    // Close mobile menu when clicking on a link
+    $('.navbar-nav > li > a').click(function() {
+        if ($(window).width() < 768) {
+            $('.navbar-collapse').collapse('hide');
+        }
+    });
+    
+    // Close mobile menu when clicking outside
+    $(document).click(function(e) {
+        if (!$(e.target).closest('.navbar').length) {
+            $('.navbar-collapse').collapse('hide');
+        }
+    });
+}
+
+// Navbar scroll effect
+$(window).scroll(function() {
+    const navbar = $('.navbar');
+    if ($(window).scrollTop() > 50) {
+        navbar.addClass('navbar-scrolled');
+    } else {
+        navbar.removeClass('navbar-scrolled');
+    }
+});
+
+// Add loading state to login link
+$('.login-link').click(function() {
+    const $this = $(this);
+    const originalText = $this.html();
+    
+    $this.html('<i class="fa fa-spinner fa-spin" aria-hidden="true"></i> Loading...');
+    
+    // Reset after 2 seconds if still on same page
+    setTimeout(function() {
+        if ($this.is(':visible')) {
+            $this.html(originalText);
+        }
+    }, 2000);
+});
+
+// Dropdown hover effect for desktop
+if ($(window).width() > 768) {
+    $('.dropdown').hover(
+        function() {
+            $(this).find('.dropdown-menu').stop(true, true).slideDown(200);
+        },
+        function() {
+            $(this).find('.dropdown-menu').stop(true, true).slideUp(200);
+        }
+    );
+}
+
+// Add navbar padding to body to account for fixed navbar
+$(document).ready(function() {
+    $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
+});
+
+// Update navbar padding on window resize
+$(window).resize(function() {
+    $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
+});
