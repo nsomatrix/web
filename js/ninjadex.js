a72fd0f2c8bb0f10e2b6247b420db906edcc233c
nsomatrix <nsomatrix@yahoo.com>
2025-10-07 20:49:48 +0530

    db

--- js/ninjadex.js
+++ js/ninjadex.js
@@ -242,12 +242,7 @@ class Ninjadex {
     }
 
     formatNumber(num) {
-        if (num >= 1000000) {
-            return (num / 1000000).toFixed(1) + 'M';
-        } else if (num >= 1000) {
-            return (num / 1000).toFixed(1) + 'K';
-        }
-        return num.toString();
+        return num.toLocaleString();
     }
 }
 
